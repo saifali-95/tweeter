e4630c145ae70f8dc2ca27c46fa663ed2bd932b8
Saif Ali <saifali7646@gmail.com>
2021-08-12 18:59:49 +0000

    add and remove class to change the color counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,13 +1,20 @@
 $(document).ready(function() {
   // --- our code goes here ---
 
-  $("#tweet-text").on('keydown', function() {
+  $("#tweet-text").on('input', function() {
     
     const $textLength = ($(this).val().length);
-    const $counter = $(this).closest('form').find('.button .counter');
+    const counter = $(this).closest('form').find('.button .counter');
+    const $currentCounter = 140-$textLength;
 
-    $counter.html(140-$textLength);
+    counter.html(140-$textLength);
 
+    if ($currentCounter < 0) {
+      $(counter).addClass( "counter-red" );
+    }
+    else {
+      $(counter).removeClass( "counter-red" );
+    }
 
   });
 
